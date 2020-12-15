3c167298d8394ca49564f200ace74a0d21f4cdfb
Tak1s <dkornienko604@gmail.com>
2020-12-15 23:46:28 +0200

    Add procfile

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const logger = require('morgan');
+const http = require('http');
+const debug = require('debug');
 
-logger('DeezerToSpotify');
+const logger = debug('DeezerToSpotify');
+const server = http.createServer((req, res) => {
+  const reqLogger = logger.extend('requestHandler');
+  const handlerMethod = {
+    POST: () => {
+      reqLogger('post');
+      const handlerUri = {
+        '/test': () => {
+          reqLogger('post');
+        }
+      };
+    },
+    GET: () => {
+      reqLogger.extend('get')(req.url);
+      const handlerUri = {
+        '/auth-callback': () => {
+          res.end('DeezerToSpotify auth callback');
+        }
+      };
+      typeof handlerUri[req.url] === 'function'
+        ? handlerUri[req.url]()
+        : res.end(`
+          <!doctype html>
+          <html>
+          <body>
+              DeezerToSpotify
+          </body>
+          </html>
+        `);
+    }
+  };
 
+  typeof handlerMethod[req.method] === 'function' && handlerMethod[req.method]();
+});
+
+server.listen(3000, () => {
+  logger('Server created!');
+});
